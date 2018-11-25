a11b34697f66a315593757553b07e019cae9b69e
luoxiaobu <luoxiaobu@users.noreply.github.com>
2018-11-25 23:04:05 +0800

    add slider

---
+++ src/components/common/a-slider/ASlider.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="a-slider">
+    <slot></slot>
+  </div>
+  <div class="a-swipe-dot" v-show="showDot">
+    <div class="a-swipe-dot-item"
+      v-for="(page, $index) in pages"
+      :class="{ 'is-active': $index === index }"></div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    props: {
+      showDot: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style lang="stylus">
+  
+</style>
--- src/service/recommend.js
+++ src/service/recommend.js
@@ -2,7 +2,7 @@
 import { jsonpRequest } from '../utils/jsonpRequest'
 import { qqyBase } from './config'
 
-// may see Cross-Origin Read Blocking (CORB) 
+// may see Cross-Origin Read Blocking (CORB)
 
 export function getRecommend (data) {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
