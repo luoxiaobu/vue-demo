ed0967b0859a61f13cdd7fbb7f3ea86a55b2a485
luoxiaobu <573567138@qq.com>
2018-11-28 19:39:41 +0800

    test

--- src/components/common/a-slider/ASlider.vue
+++ src/components/common/a-slider/ASlider.vue
@@ -8,10 +8,13 @@
 </template>
 
 <script type="text/javascript">
+  const NO_DRAG = 1;
+  const INSERT_NUM = 2;
   export default {
     data () {
       return {
-
+        noDrag: true,
+        currentIndex: 1,
       }
     },
     props: {
@@ -26,10 +29,37 @@
       interval: {
         type: Number,
         default: 4000
+      },
+      defaultIndex: {
+        type: Number,
+        default: 0
       }
     },
     methods: {
-      
+      reInitPages() {
+        var pagesTemp = [];
+        var children = this.$children;
+        var length = children.length;
+        this.noDrag = children.length === 1;
+        if (!this.noDrag) {
+          pagesTemp.push(children[0]);
+          length = length + 2;
+        }
+        children.forEach((child, index) => {
+          pagesTemp.push(child);
+          child.$el.classList.remove('is-active');
+          const c = child;
+          c.$el.style.width = `${100 / children.length}%`;
+          c.$el.style.left = `${(100 * index) / children.length}%`;
+          if (index === this.defaultIndex) {
+            child.$el.classList.add('is-active');
+          }
+        });
+        this.pages = pagesTemp;
+      },
+      initSliderItem () {
+
+      }
     }
   }
 </script>
