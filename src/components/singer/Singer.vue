64a94f2528c45d315cabb6f7e4c7238fdf0d8ffb
luoxiaobu <luoxiaobu@users.noreply.github.com>
2018-12-05 22:48:27 +0800

    add singData

--- src/components/singer/Singer.vue
+++ src/components/singer/Singer.vue
@@ -5,7 +5,69 @@
 </template>
 
 <script>
+import { getSingerList } from 'service/singer'
+import Singer from './singerData.js'
+
+const HOT_SINGER_NUM = 10;
+const HOT_NAME = 'hot'
 export default {
+  data () {
+    return {
+      singerList: []
+    }
+  },
+  methods: {
+    getSingerList () {
+      getSingerList().then((data) => {
+        this.singerList = this.normalizeSinger(data.list);
+        console.log(this.singerList)
+      }).catch(() => {})
+    },
+    normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_NUM) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  },
+  mounted () {
+    this.getSingerList();
+  }
 }
 </script>
 
