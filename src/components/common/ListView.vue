e0fa0e879104147a49d17a0d792c504966de029f
luoxiaobu <573567138@qq.com>
2018-12-07 11:57:05 +0800

    add side bar

--- src/components/common/ListView.vue
+++ src/components/common/ListView.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="list-view">
+  <a-scroll class="list-view">
     <ul>
       <li v-for="group in data" class="list-group" :key="group.title">
         <h2 class="list-group-title">{{group.title}}</h2>
-        <ul>
+        <ul class="list-group-block">
           <li class="list-group-item" v-for="(item, index) in group.items" :key="index">
             <img class="avatar" :src="item.avatar">
             <span class="name">{{item.name}}</span>
@@ -11,20 +11,38 @@
         </ul>
       </li>
     </ul>
-  </div>
+    <div class="side-bar">
+      <div class="side-bar-item" v-for="(title, index) in barData" :key="index">
+        {{title}}
+      </div>
+    </div>
+  </a-scroll>
 </template>
 <script>
+import AScroll from './AScroll'
 export default {
+  components: {
+    AScroll
+  },
   props: {
     data: {
       type: Array,
       default: () => []
     }
+  },
+  computed: {
+    barData () {
+      return this.data.map((group) => {
+        return group.title.substr(0, 1)
+      })
+    }
+  },
+  methods: {
   }
 }
 </script>
 <style lang="stylus">
-@import "../../../themes/variable"
+@import "../../themes/variable"
 .list-view {
   .list-group {}
   .list-group-title {
@@ -48,6 +66,31 @@ export default {
       margin-left: 20px;
       color: $color-text-dark;
       font-size: $font-size-medium;
+    }
+  }
+  .list-group-block {
+    padding-bottom: 20px;
+  }
+  .side-bar {
+    position: absolute;
+    z-index: 30
+    right: 0;
+    top: 50%;
+    transform: translate(0,-50%);
+    width: 20px;
+    padding: 20px 0;
+    border-radius: 10px;
+    text-align: center;
+    font-family: Helvetica;
+    background-color: transparent;
+  }
+  .side-bar-item {
+    padding: 3px;
+    line-height: 1;
+    color: $color-text-gr;
+    font-size: $font-size-small;
+    &.current {
+      color: $color-theme;
     }
   }
 }
