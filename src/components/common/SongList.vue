a76e861413a09cdb68496835f1cd28156792c235
luoxiaobu <573567138@qq.com>
2018-12-12 18:56:42 +0800

    add song list

---
+++ src/components/common/SongList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li @click="selectItem(displaySongs, index)" class="item" v-for="(song, index) in displaySongs" :key ="song.id">
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{song.displaySong}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    displaySongs () {
+      return this.songs.map((item) => {
+        var s = { ...item }
+        s.displaySong = `${item.singer}·${item.album}`;
+        return s;
+      })
+    }
+  },
+  methods: {
+    selectItem (item, index) {
+      this.$emit('select', item, index)
+    }
+  }
+}
+</script>
+<style lang="stylus">
+@import "../../themes/variable"
+@import "../../themes/mixin"
+.song-list {
+  .item {
+    display: flex;
+    align-items: center;
+    height: 64px;
+    font-size: $font-size-medium;
+    .content {
+      flex: 1;
+      line-height: 20px;
+      overflow: hidden;
+      .name {
+        no-wrap()
+        color: $color-text-dark;
+      }
+      .desc {
+        no-wrap()
+        margin-top: 4px;
+        color: $color-text-d;
+      }
+    }
+  }
+}
+</style>
--- src/components/music-list/MusicList.vue
+++ src/components/music-list/MusicList.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="music-list">
-    <div class="back">
-      <i></i>
+    <div class="back white" @click.stop="back">
+      <div class="left-botton">
+        <div class="back-arrow"></div>
+        <div class="left-title">返回</div>
+      </div>
+    </div>
+    <div class="back dark">
+      <div class="left-botton">
+        <div class="back-arrow"></div>
+        <div class="left-title">返回</div>
+      </div>
       <h1 class="title" v-html="title"></h1>
     </div>
-    <div class="bg-image" :style="bgStyle"></div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage"></div>
+    <a-scroll :top="imageHeight" class="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs" @select="selectItem"></song-list>
+      </div>
+    </a-scroll>
   </div>
 </template>
 <script>
+import aScroll from 'components/common/AScroll'
+import songList from 'components/common/SongList'
 export default {
+  data () {
+    return {
+      imageHeight: '0px'
+    }
+  },
   props: {
     bgImage: {
       type: String,
@@ -23,10 +44,22 @@ export default {
       default: ''
     }
   },
+  components: {
+    aScroll, songList
+  },
   computed: {
     bgStyle () {
       return `background-image:url(${this.bgImage})`
     }
+  },
+  methods: {
+    selectItem () {},
+    back () {
+      this.$router.back()
+    }
+  },
+  mounted () {
+    this.imageHeight = `${this.$refs.bgImage.clientHeight}px`
   }
 }
 </script>
@@ -34,16 +67,79 @@ export default {
 <style lang="stylus">
 @import "../../themes/variable"
 .music-list {
+  position: fixed
+  z-index: 100
+  top: 0
+  left: 0
+  bottom: 0
+  right: 0
   .back {
-    position: relative;
+    position: absolute;
+    left: 0;
+    right: 0;
     top: 0;
-    text-align: center;
+    line-height: 40px;
+    height: 40px;
+    z-index: 50;
+    .title {
+      font-size: $font-size-medium-x;
+      color: $color-text-d;
+    }
+    &.white {
+    }
+    &.dark {
+      display: none;
+      text-align: center;
+      background: $color-highlight-background;
+    }
+  }
+  .left-botton {
+    position: absolute;
+    .left-title {
+      font-size: $font-size-medium;
+      display: inline-block;
+      vertical-align: middle;
+      color: $color-pink;
+    }
+    .back-arrow {
+      width: 10px;
+      height: 10px;
+      transform: rotate(-45deg);
+      display: inline-block;
+      vertical-align: middle;
+      margin-left: 10px;
+      border-top: 1px solid $color-pink;
+      border-left: 1px solid $color-pink;
+    }
+  }
+  .dark {
+    .left-title {
+      color: $color-text-d;
+    }
+    .back-arrow {
+      border-top: 1px solid $color-text-d;
+      border-left: 1px solid $color-text-d;
+    }
+  }
+  .white {
+    .back-arrow {
+    }
   }
   .bg-image {
     width: 100%;
     padding-top: 70%;
     height: 0
-    background-size: cover
+    background-size: cover;
+    position: relative;
+  }
+  .list {
+    position: fixed
+    top: 0
+    bottom: 0
+    width: 100%
+  }
+  .song-list-wrapper {
+    padding: 20px 30px;
   }
 }
 </style>
