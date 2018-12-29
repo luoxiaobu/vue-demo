86347d73b9c1b86ad59670346b490feefacffc43
luoxiaobu <573567138@qq.com>
2018-12-29 18:58:58 +0800

    add progressCircle

--- src/components/a-player/APlayer.vue
+++ src/components/a-player/APlayer.vue
@@ -73,20 +73,24 @@
         <div class="background">
           <img width="100%" height="100%" :src="currentSong.image">
         </div>
-        <div class="icon">
-          <div :class="[playStatus,'singer-card']">
-            <img class="image" width="40" height="40" :src="currentSong.image">
+        <div class="mini-player-content">
+          <div class="icon">
+            <div :class="[playStatus,'singer-card']">
+              <img class="image" width="40" height="40" :src="currentSong.image">
+            </div>
           </div>
-        </div>
-        <div class="text">
-          <h2 class="name" v-html="currentSong.name"></h2>
-          <p class="desc" v-html="currentSong.singer"></p>
-        </div>
-        <div class="control">
-          <i @click.stop="togglePlaying" :class="miniIcon"></i>
-        </div>
-        <div class="control">
-          <i class="icon-playlist"></i>
+          <div class="text">
+            <h2 class="name" v-html="currentSong.name"></h2>
+            <p class="desc" v-html="currentSong.singer"></p>
+          </div>
+          <div class="control">
+            <progress-circle :radius="radius" :percent="percent" :stroke-width="strokeWidth">
+              <i @click.stop="togglePlaying" :class="miniIcon"></i>
+            </progress-circle>
+          </div>
+          <div class="control">
+            <i class="icon-playlist"></i>
+          </div>
         </div>
       </div>
     </transition>
@@ -99,6 +103,7 @@ import { SHOW_MODE } from '@/data/consts';
 import { prefixStyle } from '@/utils/tools.js';
 import { getTranslate, transitionEndEvent } from '@/utils/animation';
 import progressBar from 'components/common/ProgressBar';
+import progressCircle from 'components/common/ProgressCircle';
 const transform = prefixStyle('transform')
 const transition = prefixStyle('transition')
 export default {
@@ -107,11 +112,13 @@ export default {
       SHOW_MODE,
       songReady: false,
       playingLyric: '',
-      currentTime: 0
+      currentTime: 0,
+      radius: 30,
+      strokeWidth: 10
     }
   },
   components: {
-    progressBar
+    progressBar, progressCircle
   },
   computed: {
     ...mapGetters([
@@ -132,6 +139,9 @@ export default {
     },
     playIcon () {
       return this.playing ? 'icon-pause' : 'icon-play'
+    },
+    percent () {
+      return this.currentTime / this.currentSong.duration
     }
   },
   watch: {
@@ -404,18 +414,20 @@ export default {
     position: fixed;
     left: 0;
     bottom: 0;
-    z-index: 180;
     width: 100%;
-    height: 60px;
-    display: flex;
-    align-items: center;
+    z-index: 1;
     background: #fff;
+    .mini-player-content {
+      height: 60px;
+      display: flex;
+      align-items: center;
+    }
     .background {
       opacity: 0.8;
     }
     .icon {
       width: 80px
-      padding: 0 10px 0 20px
+      padding: 0 20px;
     }
     .text {
       display: flex;
