4cce09d340f536efd1071f806ba1202e7bc6e9e0
luoxiaobu <luoxiaobu@users.noreply.github.com>
2019-01-29 09:11:41 +0800

    modify

--- src/components/search-result/SearchResult.vue
+++ src/components/search-result/SearchResult.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="search-result">
     <ul class="search-list">
-      <li></li>
+      <li class="search-item" v-for="item in result">
+        <div class="icon"></div>
+        <p class="main-title"></p>
+        <p class="sub-title"></p>
+      </li>
     </ul>
   </div>
 </template>
 <script>
 import AScroll from 'components/common/AScroll'
+import { getSingerList } from 'service/singer';
 export default {
-
+  data () {
+    return {
+      result: []
+    }
+  },
+  methods: {
+    search () {
+      search().then((data) => {
+        console.log()
+      }).catch((data) => {})
+    },
+  },
+  created () {
+    this.search();
+  }
 }
 </script>
 
-<style scoped lang="stylus">
-  
+<style lang="stylus">
+  .search-result {
+    .search-item {
+      position: relative;
+      height: 55px;
+      padding-left: 56px;
+      overflow: hidden;
+    }
+  }
 </style>
