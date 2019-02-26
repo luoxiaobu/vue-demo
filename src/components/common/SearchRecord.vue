096a4e92eaee05e5e8fce2ca3fc74e88b3058276
luoxiaobu <573567138@qq.com>
2019-02-26 18:47:50 +0800

    add searchRecod

---
+++ src/components/common/SearchRecord.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-record">
+    <ul>
+      <li>
+        <a href="javascript:;" class="record-main">
+          <span class="icon iocn-clock"></span>
+          <span class="record-text">我曾把完整的镜子打碎</span>
+          <span class="icon-close"></span>
+        </a>
+      </li>
+    </ul>
+    <p class="record-handle">
+      <a href="javascript:;">清除搜索记录</a>
+    </p>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+}
+</script>
+<style lang="stylus">
+@import "../../themes/variable"
+@import "../../themes/mixin"
+.search-record {
+
+}
+</style>
