86d3f231b02d55b85de373fdb542a8dd60e157fb
luoxiaobu <luoxiaobu@users.noreply.github.com>
2019-03-10 22:14:58 +0800

    cancel axios

--- src/utils/axiosRequest.js
+++ src/utils/axiosRequest.js
@@ -7,6 +7,7 @@ import axios from 'axios';
  */
 export function axiosRequest (config = {}) {
   var axiosIns = axios.create(config);
+  var CancelToken = axios.CancelToken;
   axiosIns.interceptors.response.use((res) => {
     // need refer to the data provided by the server
     var data = res.data || res;
@@ -17,11 +18,16 @@ export function axiosRequest (config = {}) {
     return Promise.reject(data);
   });
   let request = (url, options = {}) => {
+    var cancel = null
     let promise = new Promise((resolve, reject) => {
       options.headers = options.headers || {};
       options.url = url;
       options.method = options.method || 'get';
       options.timeout = options.timeout || 20000;
+      options.cancelToken = new CancelToken(function executor (c) {
+        cancel = c;
+        // 这个参数 c 就是CancelToken构造函数里面自带的取消请求的函数，这里把该函数当参数用
+      })
       let send = () => {
         axiosIns.request(options).then((data) => {
           resolve(data);
@@ -31,6 +37,7 @@ export function axiosRequest (config = {}) {
       };
       send();
     });
+    promise.cancel = cancel;
     return promise;
   };
   request.interceptors = axiosIns.interceptors;
